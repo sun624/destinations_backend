0c29ed00fb4213a2e516fe82f840190078103d79
sun624 <67940809+sun624@users.noreply.github.com>
2021-04-23 16:06:18 -0700

    revert 2

--- index.js
+++ index.js
@@ -20,19 +20,21 @@ server.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}.`);
 });
 
-// server.get("/", (req, res) => {
-//   res.send(db);
-// });
+server.get("/", (req, res) => {
+  res.send(db);
+  //console.log("hello from /");
+
+});
 
 //GET / best practice is to use query parameters
 server.get("/", (req, res) => {
   //res.send(db);
   //console.log("hello from /");
-  const { location } = req.query;
-  if (!location) return res.send(db);
+  const {location} = req.query;
+  if (!location) return res.status(400).json({ error: "need location" });
+
   const locations = db.filter((place) => place.location === location);
-  // res.send(locations);
-  res.send(locations)
+  res.send(locations);
 });
 
 // server.get("/:location",(req,res)=>{
@@ -53,17 +55,16 @@ server.post("/", async (req, res) => {
 
   const uid = getUID();
   const photo = await getPhoto(name);
-  const newLocation = {
+
+  db.push({
     uid,
     name,
     location,
     photo,
     description: description || "",
-  };
-  db.push(newLocation);
+  });
 
-  // res.send(newLocation);
-  res.redirect("/");
+  res.send({ uid });
   console.log(db);
 });
 
@@ -71,43 +72,40 @@ server.post("/", async (req, res) => {
 
 server.put("/", async (req, res) => {
   console.log("Inside PUT");
-  const { uid } = req.query;
+  const  {uid}  = req.query;
   console.log(uid);
 
-  if (!uid || uid.length !== 6)
-    return res.status(400).json({ error: "uid is not right" });
-  const { name, location, description } = req.body;
+  if(!uid || uid.length !== 6) return res.status(400).json({error:"uid is not right"});
+  const {name,location, description } = req.body;
 
   if (!name && !location && !description) {
-    return res
-      .status(400)
-      .json({ error: "need at least one property to update" });
+    return res.status(400).json({error:"need at least one property to update"})
   }
 
   // find the place in database which has the same uid from query
-  const place = db.find((place) => place.uid === uid);
-  place.description = description ? description : place.description;
+  const place = db.find(place =>place.uid === uid)
+  place.description = description? description:place.description;
   place.location = location ? location : place.location;
-
-  if (name) {
-    const photo = await getPhoto(name);
+  
+  if (name){
+    const photo = await getPhoto(name)
     place.name = name;
     place.photo = photo;
   }
   res.send("Success");
 });
 
-//DELETE
-server.delete("/", (req, res) => {
+//DELETE 
+server.delete("/",(req,res)=>{
   console.log("INside DElete");
-  const { uid } = req.query;
-
-  const index = db.findIndex((place) => place.uid === uid);
-
-  if (index > -1) {
+  const {uid} = req.query;
+  
+  const index = db.findIndex(place => place.uid === uid);
+ 
+  if(index >-1 ){
     db.splice(index, 1);
     res.send("Success");
-  } else {
-    res.send({ error: "Item not found" });
+  }else {
+    res.send({error: "Item not found"})
   }
-});
+})
